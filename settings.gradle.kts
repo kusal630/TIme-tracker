afb4fb121bfd283a8a646163fc48df3932bc4962
DailyTrack Agent <kottekvnkusal@gmail.com>
2026-08-28 21:31:51 +0530

    fix: remove foojay-resolver for F-Droid compatibility

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,9 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
-}
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
 }
 
 dependencyResolutionManagement {
@@ -23,5 +14,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "DailyTrack"
+rootProject.name = "SoulTrack"
 include(":app")
